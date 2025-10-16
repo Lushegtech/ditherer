8cf751756564f699a107b64730f4e060b02aedfd
Elijah Olusegun Adebayo <olusegunvicky121@gmail.com>
2025-10-16 11:44:53 +0100

    Add .gitignore and enhance gradient example

--- examples/generate_gradient.py
+++ examples/generate_gradient.py
@@ -1,9 +1,13 @@
 from PIL import Image
 import numpy as np
 
-width, height = 256, 128
+width, height = 512, 256
 
-x = np.linspace(0, 1, width, dtype=np.float32)
+x = np.linspace(0, 1, width, dtype = np.float32)
 grad = np.tile(x[None, :], (height, 1))
 
-img = np.stack([grad, grad**0.5, grad**2], axis=-1)
+img = np.stack([grad**1.5, np.sqrt(grad), np.sin(grad * np.pi * 0.5)], axis=-1)
+
+Image.fromarray((img * 255 + 0.5).astype(np.uint8), "RGB").save("examples/gradient.png")
+
+print("Gradient saved to examples/gradient.png")
