b165af40910bb5e5272337069504b91c93618743
Elijah Olusegun Adebayo <olusegunvicky121@gmail.com>
2025-10-16 19:20:19 +0100

    chore: improve code readability in gradient generator

--- examples/generate_gradient.py
+++ examples/generate_gradient.py
@@ -6,8 +6,13 @@ width, height = 512, 256
 x = np.linspace(0, 1, width, dtype = np.float32)
 grad = np.tile(x[None, :], (height, 1))
 
-img = np.stack([grad**1.5, np.sqrt(grad), np.sin(grad * np.pi * 0.5)], axis=-1)
+red = grad ** 1.5
+blue = np.sqrt(grad)
+green = np.sin(grad * np.pi * 0.5)
 
-Image.fromarray((img * 255 + 0.5).astype(np.uint8), "RGB").save("examples/gradient.png")
+img = np.stack([red, green, blue], axis=-1)
 
-print("Gradient saved to examples/gradient.png")
+img_grad = Image.fromarray((img * 255 + 0.5).astype(np.uint8), "RGB")
+img_grad.save("examples/gradient.png")
+
+print("Your generated gradient has been saved to examples/gradient.png")
