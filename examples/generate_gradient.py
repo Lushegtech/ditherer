9a74cf1bc4d5465ea25a73663bd5dd0750a4767c
Elijah Olusegun Adebayo <olusegunvicky121@gmail.com>
2025-10-21 21:23:00 +0100

    chore: add initialization file

--- examples/generate_gradient.py
+++ examples/generate_gradient.py
@@ -12,6 +12,10 @@ green = np.sin(grad * np.pi * 0.5)
 
 img = np.stack([red, green, blue], axis=-1)
 
+print(img.ndim)
+print(img.shape)
+print(img.dtype)
+
 img_grad = Image.fromarray((img * 255 + 0.5).astype(np.uint8), "RGB")
 img_grad.save("examples/gradient.png")
 
---
+++ intit.py
@@ -0,0 +1,3 @@
+
+__all__ = ["cli", "color", "palette"]
+__version__ = "0.1.0"
